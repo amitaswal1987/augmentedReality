48829b266f8910f518ae31ad861172ddaa36df23
RAMAN <bharat.malhotra@mobileprogramming.net>
2018-02-08 15:59:17 +0530

    Code created

---
+++ AugmentedReality/AugmentedReality/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AugmentedReality
+//
+//  Created by Amit Aswal on 2/8/18.
+//  Copyright © 2018 Mobile Programming LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
